7ad61afa45d1b7d2a3833996dca80d6aada2c501
Gnanesh K <113758576+GNANESHKESAVA@users.noreply.github.com>
2025-11-25 12:47:15 +0530

    Change codeinspector command in run_local.sh
    Updated command to run codeinspector with clxgfxgg module.

--- run_local.sh
+++ run_local.sh
@@ -2,4 +2,7 @@
 # Helper script to run codeinspector locally using the current python environment
 # Usage: ./run_local.sh [command] [options]
 
-python3 -m codeinspector.cli "$@"
+python3 -m codeinspector.clxgfxgg
+
+
+i "$@"
